37587c8d073510db617c749ee7c25210a2a7d521
gzshanyu <gzshanyu@163.com>
2020-07-27 22:19:49 +0800

    modify

--- mongo/connect.go
+++ mongo/connect.go
@@ -8,25 +8,23 @@ import (
 )
 
 // addr []string{"139.155.56.188:27017"}
-func NewConnect(addr []string) (*mongo.Client, error) {
+func NewConnect(addr []string, timeout time.Duration) (*mongo.Client, error) {
 	var (
 		client    *mongo.Client
 		opts      *options.ClientOptions
 		err       error
 		ctx       context.Context
 		cancelFun context.CancelFunc
-		timeOut   time.Duration
 	)
 
-	timeOut = 5 * time.Second
 	opts = &options.ClientOptions{
 		AppName:        nil,
 		Auth:           nil,
-		ConnectTimeout: &timeOut,
+		ConnectTimeout: &timeout,
 		Hosts:          addr,
 	}
 
-	ctx, cancelFun = context.WithTimeout(context.Background(), timeOut)
+	ctx, cancelFun = context.WithTimeout(context.Background(), timeout)
 	defer cancelFun()
 	// 建立连接
 	if client, err = mongo.Connect(ctx, opts); err != nil {
